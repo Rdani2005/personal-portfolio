c17adb0eab7bf44583ff44b18ab66cab386153a6
rdani2005 <dricardosc15@gmail.com>
2022-10-14 14:53:12 -0600

    Secret Key configs

--- core/settings/production.py
+++ core/settings/production.py
@@ -3,9 +3,11 @@ from .base import *  # NOQA
 from .base import env
 
 # Base
-SECRET_KEY = env('DJANGO_SECRET_KEY')
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=[
-                         'comparteride.com', '18.188.154.88'])
+SECRET_KEY = env('SECRET_KEY')
+ALLOWED_HOSTS = [
+    'comparteride.com',
+    '18.188.154.88'
+]
 
 # Databases
 DATABASES['default'] = {
